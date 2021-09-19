28e754a838ba7e6dee5e892056e2d234afc5d287
Bar Ganor <70594071+BarGanor@users.noreply.github.com>
2021-09-19 20:24:52 +0300

    server commit

---
+++ server/db-files/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: 'Bg8187915',
+    DB: 'cinema_db'
+};
---
+++ server/myApp.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require("path");
+const sql = require('./db-files/db.js');
+const CRUD_operations = require("./CRUD_functions/CRUD_functions.js");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true
+}));
+const port = 8080;
+
+
+app.use('/', express.static(path.join(__dirname, '../client')))
+
+
+// get customer by name
+app.post("/newMovies/:genre", CRUD_operations.getNewMovies);
+
+app.get('/cities', CRUD_operations.getDistinctCities)
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+
+})
