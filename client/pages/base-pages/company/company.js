17fd0d67a6d3723092bb40b515e1a02e65d74a1f
elyahu120 <elyahu120@gmail.com>
2021-10-05 02:54:51 +0300

    team + movies, shows

---
+++ client/pages/base-pages/company/company.js
@@ -0,0 +1,9 @@
+class getCompany {
+    constructor() {
+        this.container = document.getElementById('container');
+    }
+
+    render() {
+        this.container.innerHTML = ''
+    }
+}
---
+++ client/pages/base-pages/team/team.js
@@ -0,0 +1,32 @@
+class getTeam {
+    constructor() {
+        this.container = document.getElementById('container');
+    }
+
+    render() {
+        this.container.innerHTML =
+            '<!DOCTYPE html>\n' +
+            '<html>\n' +
+            '<head>\n' +
+            '<meta name="viewport" content="width=device-width, initial-scale=1">\n' +
+            '<style>\n' +
+            'img {\n' +
+            '  display: block;\n' +
+            '  margin-left: auto;\n' +
+            '  margin-right: auto;\n' +
+            ' border-radius: 15px;\n' +
+            '}\n' +
+            '</style>\n' +
+            '</head>\n' +
+            '<body>\n' +
+            '\n' +
+            '\n' +
+            '\n' +
+            '<br>' +
+            '<br>' +
+            '<img src="pages/pictures/all.png" alt="Paris" style="width:40%;">\n' +
+            '\n' +
+            '</body>\n' +
+            '</html>\n'
+    }
+}
